0eadeeca1d56460d70f5c5ce08ab01e016c30f28
MdodoIng <mdodoyt2@gmail.com>
2023-01-23 16:33:46 +0530

    modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,6 @@ export const Head = () => (
       content="A wide range of jewelry that can elevate every occasion to grandeur, which is the embodiment of purity and elegance. Not only your jewelry will speak to your beauty, but it will be mirrored in the radiance of your eyes."
     />
     <meta name="theme-color" content="#06231A" />
-    <meta name="theme-color" content="#06231A" />
     <meta name="msapplication-TileColor" content="#06231A" />
     <meta name="msapplication-navbutton-color" content="#06231A" />
     <meta name="apple-mobile-web-app-status-bar-style" content="#06231A" />
