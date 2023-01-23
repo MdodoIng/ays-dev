58506cf0aa0b1564b274d36aa4f73ad743802594
MdodoIng <mdodoyt2@gmail.com>
2023-01-23 12:41:29 +0530

    modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import * as React from "react";
-import { Footer, Navbar, HeroSection, OurLegacy, AuraDiamond, GoldInvestment, PromiseYourPurity, Testimonial, ContactUs } from "../components";
+import {
+  Footer,
+  Navbar,
+  HeroSection,
+  OurLegacy,
+  AuraDiamond,
+  GoldInvestment,
+  PromiseYourPurity,
+  Testimonial,
+  ContactUs,
+} from "../components";
 
 const IndexPage = () => {
   return (
@@ -11,7 +21,7 @@ const IndexPage = () => {
       <GoldInvestment />
       <PromiseYourPurity />
       <Testimonial />
-      <ContactUs/>
+      <ContactUs />
       <Footer />
     </main>
   );
@@ -19,4 +29,17 @@ const IndexPage = () => {
 
 export default IndexPage;
 
-export const Head = () => <title>AYS JEWELS</title>;
+export const Head = () => (
+  <React.Fragment>
+    <title>AYS JEWELS</title>
+    <meta
+      name="description"
+      content="A wide range of jewelry that can elevate every occasion to grandeur, which is the embodiment of purity and elegance. Not only your jewelry will speak to your beauty, but it will be mirrored in the radiance of your eyes."
+    />
+    <meta name="theme-color" content="#06231A" />
+    <meta name="theme-color" content="#06231A" />
+    <meta name="msapplication-TileColor" content="#06231A" />
+    <meta name="msapplication-navbutton-color" content="#06231A" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="#06231A" />
+  </React.Fragment>
+);
