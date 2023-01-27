a3b11186a4b328a0387bdccc1bd854cb445e21f1
MdodoIng <mdodoyt2@gmail.com>
2023-01-27 15:33:53 +0530

    modified:   gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,15 @@ module.exports = {
   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/assets/images/icon.png"
+      icon: "src/assets/images/icon.png",
+      name: `AYS JEWELS`,
+      short_name: `AYS JEWELS`,
+      description: `A wide range of jewelry that can elevate every occasion to grandeur, which is the embodiment of purity and elegance. Not only your jewelry will speak to your beauty, but it will be mirrored in the radiance of your eyes.`,
+      lang: `en`,
+      display: `standalone`,
+      start_url: `/`,
+      background_color: `#06231A`,
+      theme_color: `#fff`
     }
   }, "gatsby-plugin-sharp", "gatsby-transformer-sharp",'gatsby-plugin-offline' , {
     resolve: `gatsby-source-filesystem`,
