8e96b4e82f16dbb2aee96dedf40b4a31548af3ec
MdodoIng <mdodoyt2@gmail.com>
2023-01-27 15:27:55 +0530

    modified:   gatsby-browser.js 	modified:   gatsby-config.js 	modified:   package-lock.json 	modified:   package.json

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,16 @@ import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
+
+export const registerServiceWorker = () => true
